e5ce5f2334b30e4abb754d3249eb03551a6a69c6
Hashrocket <jasshultz@gmail.com>
2016-06-23 08:48:19 -0600

    custom pipes and chaining pipes

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {ReversePipe} from "./reverse.pipe";
 
 @Component({
     selector: 'my-app',
@@ -27,9 +28,25 @@ import {Component} from '@angular/core';
             <div>Currency: {{1.0 * inputNumberPipes.value | currency:currency.value:currencyShort.checked:'1.2-2'}}</div>
             <input type="checkbox" #currencyShort checked (change)="0">Short currency code
         </section>
+        <section class="pipe">
+            <h2>Chaining multiple Pipes (e.g. uppercase and slice)</h2>
+            <input type="text" #inputChainPipes (keyup)="0"><br/>
+            <div>Output: {{inputChainPipes.value | slice:1:3 | uppercase}}</div>
+            
+        </section>
         
-       
+        <section class="pipe">
+            <h2>Custom Pipe (reverse string)</h2>
+            <input type="text" #inputCustomPipe (keyup)="0"><br/>
+            <div>Output: {{inputCustomPipe.value | myReverse}}</div>
+        </section>
+        
+        <section class="pipe">
+            <h2>Async (stateful) pipes</h2>
+            <div>Output (received after 2s): XX</div>
+        </section>
     `,
+    pipes: [ReversePipe]
 })
 export class AppComponent {
     today = Date.now();
